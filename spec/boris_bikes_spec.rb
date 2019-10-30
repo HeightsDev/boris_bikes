3d803920fa6c2eb5b0a6c0971c441e9bec1a3e7c
Elliot <elliotsuarez@gmail.com>
2019-10-30 14:34:08 +0000

    First stage errors

---
+++ spec/boris_bikes_spec.rb
@@ -0,0 +1,13 @@
+require './lib/boris_bikes'
+
+describe DockingStation do
+it 'creates Docking Station' do
+  dockingstation = DockingStation.new
+end
+ it 'release_bike' do
+   dockingstation = DockingStation.new
+   expect(dockingstation).to respond_to(:release_bike)
+end
+
+
+end
